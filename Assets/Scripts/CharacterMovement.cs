ad282901640f7c0f8b1796be47998dff0e08f806
Matheus Assis <assis.matheus1@gmail.com>
2020-01-18 23:57:38 -0300

    creating basic components

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    [Header("Movement Variables")]
+    public float speed;
+    public float jumpSpeed;
+    public float gravity;
+    public bool isPlayable;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        if (isPlayable)
+        {
+            if (characterController.isGrounded)
+            {
+                moveDirection = new Vector3(Input.GetAxis("Horizontal"), .0f, Input.GetAxis("Vertical"));
+                moveDirection *= speed;
+
+                if (Input.GetButton("Jump"))
+                {
+                    moveDirection.y = jumpSpeed;
+                }
+            }
+
+            moveDirection.y -= gravity * Time.deltaTime;
+            characterController.Move(moveDirection * Time.deltaTime);
+        }
+        else
+        {
+            moveDirection.x = .0f;
+            moveDirection.z = .0f;
+            moveDirection.y -= gravity * Time.deltaTime;
+
+            characterController.Move(moveDirection * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    [Header("Playable Players")]
+    public GameObject ALFONSO;
+    public GameObject BARNEY;
+
+
+    private Camera cameraALFONSO;
+    private Camera cameraBARNEY;
+    private CharacterMovement cmALFONSO;
+    private CharacterMovement cmBARNEY;
+
+    void Start()
+    {
+        cameraALFONSO = ALFONSO.transform.GetChild(0).GetComponent<Camera>();
+        cameraBARNEY = BARNEY.transform.GetChild(0).GetComponent<Camera>();
+
+        cmALFONSO = ALFONSO.GetComponent<CharacterMovement>();
+        cmBARNEY = BARNEY.GetComponent<CharacterMovement>();
+    }
+
+    public void ChangePlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Time.timeScale = .5f;
+
+            if (cmALFONSO.isPlayable)
+            {
+                cmALFONSO.isPlayable = false;
+                cameraALFONSO.enabled = false;
+
+                cmBARNEY.isPlayable = true;
+                cameraBARNEY.enabled = true;
+
+                Time.timeScale = 1f;
+            }
+            else
+            {
+                cmALFONSO.isPlayable = true;
+                cameraALFONSO.enabled = true;
+
+                cmBARNEY.isPlayable = false;
+                cameraBARNEY.enabled = false;
+
+                Time.timeScale = 1f;
+            }
+        }
+    }
+
+
+    void Update()
+    {
+        ChangePlayer();
+    }
+}
